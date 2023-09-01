adba5d9c5f00c89632e8dc0b550b8ede2562d7bd
PrudhviBharadwajM <prudhvi.maale@gmail.com>
2023-09-01 18:12:52 -0400

    initial commit to the repo.

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+let port = 3000;
+let host = 'localhost';
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(port, host, () => {
+    console.log('Hello World');
+});
